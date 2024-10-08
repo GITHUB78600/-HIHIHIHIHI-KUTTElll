425c54f921fd62ea3122c19599287f1ff061846f
GITHUB78600 <githup3@yandex.com>
2024-10-08 17:43:03 +0530

    Update 0_ARIF-MISS-YOU.js

--- scripts/cmds/0_ARIF-MISS-YOU.js
+++ scripts/cmds/0_ARIF-MISS-YOU.js
@@ -1,22 +1,81 @@
-module.exports = {
- config: {
-   name: "miss you",
-   version: "1.0",
-   author: "MR.ARIF",
-   countDown: 5,
-   role: 0,
-   shortDescription: "no prefix",
-   longDescription: "no prefix",
-   category: "no prefix",
- },
+const fs = require("fs-extra");
+const axios = require("axios");
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "miss you") {
- return message.reply({
- body: "𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐌𝐀𝐑𝐈 𝐉𝐀𝐀𝐍 😇",
- attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/G34p5fzq/MISS-YOU.gif")
- });
- }
- }
+// Function to generate random message
+function generateRandomMessage(messages) {
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return messages[randomIndex];
 }
+
+module.exports = {
+  config: {
+    name: "miss you",
+    version: "1.0",
+    author: "ARIF BABU",
+    countDown: 5,
+    role: 0,
+    shortDescription: "no-prefix",
+    longDescription: "Bot aapko hindi mein jawab dega",
+    category: "non-prefix",
+    guide: {
+      en: "{p}{n}",
+    }
+  },
+
+  onStart: async function ({ }) { },
+
+  onChat: async function ({ api, event, args, Threads, userData }) {
+    const { threadID, senderID } = event;
+    // Fetch sender details
+    const senderInfo = await api.getUserInfo(senderID);
+    // Get sender name from sender details
+    const senderName = senderInfo[senderID].name;
+
+    // Trigger words and their corresponding replies and GIF links
+    const triggers = {
+      "good morning": {
+        options: ["miss you", "miss", "MISS YOU", "MISS", "i miss you"],
+        gifLinks: [
+         "https://i.postimg.cc/GhDBGBkD/10.gif",
+
+"https://i.postimg.cc/c4YqQ3Xf/11.gif",
+
+"https://i.postimg.cc/6QQtL37w/12.gif",
+
+"https://i.postimg.cc/xT0mDvhP/MISS-YOU.gif",          
+          // Add more GIF links here as per your requirement
+        ],
+        replies: ["𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 😇, " + senderName + " 𝐌𝐀𝐑𝐈 𝐉𝐀𝐀𝐍 😘", "𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 😇, " + senderName + " 𝐌𝐀𝐑𝐈 𝐉𝐀𝐀𝐍 😘", "𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 😇, " + senderName + " 𝐌𝐀𝐑𝐈 𝐉𝐀𝐀𝐍 😘"]
+      }
+    };
+
+    // Check if message body contains any trigger words
+    for (const trigger in triggers) {
+      if (triggers[trigger].options.some(option => new RegExp(`\\b${option}\\b`, 'i').test(event.body))) {
+        const { gifLinks, replies } = triggers[trigger];
+
+        // Generate random GIF link
+        const gifLink = gifLinks[Math.floor(Math.random() * gifLinks.length)];
+        // Generate random message
+        const replyMessage = generateRandomMessage(replies);
+
+        try {
+          // Fetch GIF data
+          const gifData = await axios.get(gifLink, { responseType: "stream" });
+          // Send GIF and message as attachments
+          api.sendMessage({
+            attachment: gifData.data,
+            body: replyMessage,
+            mentions: [{ tag: senderID, id: senderID }]
+          }, threadID);
+        } catch (error) {
+          console.error("GIF fetch karne mein error:", error);
+        }
+
+        return; // Message bhejne ke baad loop se bahar nikalna
+      }
+    }
+
+    // Agar koi trigger word nahi milta, to kuch nahi karna
+  }
+};
