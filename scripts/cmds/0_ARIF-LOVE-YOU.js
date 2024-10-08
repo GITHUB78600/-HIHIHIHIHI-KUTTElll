d243e7b86b3bff046a1d0cb5bbbf37371ea8ac93
GITHUB78600 <githup3@yandex.com>
2024-10-08 17:45:43 +0530

    Update 0_ARIF-LOVE-YOU.js

--- scripts/cmds/0_ARIF-LOVE-YOU.js
+++ scripts/cmds/0_ARIF-LOVE-YOU.js
@@ -1,22 +1,81 @@
-module.exports = {
- config: {
-   name: "love you",
-   version: "1.0",
-   author: "MR.ARIF",
-   countDown: 5,
-   role: 0,
-   shortDescription: "no prefix",
-   longDescription: "no prefix",
-   category: "no prefix",
- },
+const fs = require("fs-extra");
+const axios = require("axios");
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "love you") {
- return message.reply({
- body: "𝑰 𝑳𝑶𝑽𝑬 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑴𝑬𝑹𝑰 𝑱𝑨𝑨𝑵 🙈",
- attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/x8hKfDBn/LOVE-YOU.gif")
- });
- }
- }
+// Function to generate random message
+function generateRandomMessage(messages) {
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return messages[randomIndex];
 }
+
+module.exports = {
+  config: {
+    name: "love you",
+    version: "1.0",
+    author: "ARIF BABU",
+    countDown: 5,
+    role: 0,
+    shortDescription: "no-prefix",
+    longDescription: "Bot aapko hindi mein jawab dega",
+    category: "non-prefix",
+    guide: {
+      en: "{p}{n}",
+    }
+  },
+
+  onStart: async function ({ }) { },
+
+  onChat: async function ({ api, event, args, Threads, userData }) {
+    const { threadID, senderID } = event;
+    // Fetch sender details
+    const senderInfo = await api.getUserInfo(senderID);
+    // Get sender name from sender details
+    const senderName = senderInfo[senderID].name;
+
+    // Trigger words and their corresponding replies and GIF links
+    const triggers = {
+      "good morning": {
+        options: ["love", "I love", "I love you", "LOVE", "I LOVE"],
+        gifLinks: [
+         "https://i.postimg.cc/BQ4kqRqD/90c2c7a3a1396f43b4279e520be3c79d.gif",
+
+"https://i.postimg.cc/d0DNDTSc/2.gif",
+
+"https://i.postimg.cc/wMKsLSqQ/3.gif",
+
+"https://i.postimg.cc/rFVYrbDk/LOVE-YOU.gif",          
+          // Add more GIF links here as per your requirement
+        ],
+        replies: ["𝑰 𝑳𝑶𝑽𝑬 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑴𝑬𝑹𝑰 🙈, " + senderName + " 𝑱𝑨𝑨𝑵😘", "𝑰 𝑳𝑶𝑽𝑬 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑴𝑬𝑹𝑰 🙈, " + senderName + " 𝑱𝑨𝑨𝑵 😘", "𝑰 𝑳𝑶𝑽𝑬 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑴𝑬𝑹𝑰 🙈, " + senderName + " 𝑱𝑨𝑨𝑵 😘"]
+      }
+    };
+
+    // Check if message body contains any trigger words
+    for (const trigger in triggers) {
+      if (triggers[trigger].options.some(option => new RegExp(`\\b${option}\\b`, 'i').test(event.body))) {
+        const { gifLinks, replies } = triggers[trigger];
+
+        // Generate random GIF link
+        const gifLink = gifLinks[Math.floor(Math.random() * gifLinks.length)];
+        // Generate random message
+        const replyMessage = generateRandomMessage(replies);
+
+        try {
+          // Fetch GIF data
+          const gifData = await axios.get(gifLink, { responseType: "stream" });
+          // Send GIF and message as attachments
+          api.sendMessage({
+            attachment: gifData.data,
+            body: replyMessage,
+            mentions: [{ tag: senderID, id: senderID }]
+          }, threadID);
+        } catch (error) {
+          console.error("GIF fetch karne mein error:", error);
+        }
+
+        return; // Message bhejne ke baad loop se bahar nikalna
+      }
+    }
+
+    // Agar koi trigger word nahi milta, to kuch nahi karna
+  }
+};
